b55408798b484956fdce9c745fb621ee67df228a
Babakailash <nathkailash2020@gmail.com>
2021-04-21 09:32:21 +0530

    EmpWage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,1 +1,14 @@
-#Welcome message for 
+#!/bin/bash -x
+
+
+empCheck=$((RANDOM%2))
+
+if  [ $empCheck -eq 1 ]
+then
+        empWage=20;
+        empHrs=10;
+      echo =$(($empWage*$empHrs))
+
+else 
+        echo  =0;
+fi
