7b5db3cae7ee2e31e28407a27751eb4b07c0ec17
Babakailash <nathkailash2020@gmail.com>
2021-04-21 10:46:05 +0530

    Added: Emp-PartTime-Wage

--- empPartTimeWage.sh
+++ empPartTimeWage.sh
@@ -1,20 +1,18 @@
 #!/bin/bash -x
 
 isPartTime=1;
-isfullTime=2;
+isFullTime=2;
 empRatePerHr=20;
-empCheck=$((RANDOM%3));
+randomCheck=$((RANDOM%3));
 
-case $empCheck in
-   $isFullTime)
-         empHrs=8
-         ;;
-    $isPartTime)
-         empHrs=4
-         ;;
-      *)
-        empHrs=0
-          ;;
- esac
-    
+if [ $isFullTime -eq $randomCheck ];
+then
+            empHrs=8;
+elif [ $isPartTime -eq $randomCheck ];
+then
+            empHrs=4;
+else
+            empHrs=0;
+fi
+
      salary=$(($empHrs*$empRatePerHr));
